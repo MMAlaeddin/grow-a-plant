2d21ad41d2143d0fec5bfc337091d83de4663a3f
Student <example@example.com>
2020-02-27 10:13:04 -0800

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace grow_your_own_plant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
